2bc5eaa826af64de9bb273fd044bde57de5e72de
X1 <sudiptodas333999@gmail.com>
2025-09-19 08:07:04 +0530

    day1

--- src/main/java/com/leetcode/Main.java
+++ src/main/java/com/leetcode/Main.java
@@ -1,6 +1,6 @@
 package com.leetcode;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//lk//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
